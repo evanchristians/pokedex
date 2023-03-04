1f04eff6bace7ef1fcb18623a45a18302ba8d183
evanchristians <evanryk@gmail.com>
2023-03-04 16:19:33 +0100

    feat: more styling

--- src/App.tsx
+++ src/App.tsx
@@ -12,31 +12,33 @@ function App() {
   return (
     <main className="bg-slate-100 text-slate-600 px-4 flex flex-col items-center min-h-screen">
       <header className="flex justify-center w-full py-4">
-        <div className="max-w-screen-xl w-full px-4">
+        {/* <div className="max-w-screen-xl w-full px-4">
           <img src="/pokedex.png" alt="Pokedex Logo" className="max-w-[6rem]" />
-        </div>
+        </div> */}
       </header>
       <div className="p-5"></div>
       {loading && !pokemon && <p>Loading...</p>}
       {error && !pokemon && <p>{error.message}</p>}
       {pokemon && (
-        <div className="flex gap-6 w-full max-w-screen-xl px-4">
-          <div className="w-full grid grid-cols-3 gap-y-12 gap-x-6 col-span-2 place-content-start">
-            <div className="col-span-3 flex gap-4 px-4 py-2 rounded-2xl bg-white">
-              <MagnifyingGlassIcon className="w-5 text-slate-400"/>
-              <input
-                type="text"
-                placeholder="Search..."
-                className="focus:outline-none w-full"
-                value={searchQuery}
-                onChange={(evt) => setSearchQuery(evt.target.value)}
-              />
+        <div className="flex grow gap-6 w-full max-w-screen-xl px-4">
+          <div className="w-full grid sm:grid-cols-3 gap-y-6 sm:gap-y-12 gap-x-6 col-span-2 place-content-start">
+            <div className="sm:col-span-3">
+              <div className="flex max-w-sm text-xs gap-4 px-4 py-2 rounded-lg sm:rounded-2xl bg-indigo-500 text-white">
+                <MagnifyingGlassIcon className="w-5" />
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="focus:outline-none w-full bg-transparent placeholder:text-white"
+                  value={searchQuery}
+                  onChange={(evt) => setSearchQuery(evt.target.value)}
+                />
+              </div>
             </div>
             {pokemon.map((p) => (
               <PokeCard key={p.id} pokemon={p} />
             ))}
           </div>
-          <aside className="w-full max-w-sm">
+          <aside className="w-full max-w-sm hidden sm:block">
             <ShowcasePokemon />
           </aside>
         </div>
--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -5,13 +5,21 @@ import { selectedPokemonAtom } from "../store/pokemon";
 import TypeBadge from "./TypeBadge";
 
 const PokeCard = ({ pokemon }: { pokemon: Pokemon }) => {
-  const [_, setSelectedPokemon] = useAtom(selectedPokemonAtom);
+  const [selectedPokemon, setSelectedPokemon] = useAtom(selectedPokemonAtom);
+  const shadows =
+    "shadow-2xl shadow-slate-200 hover:shadow-slate-300";
+  const container =
+    "cursor-pointer bg-white border border-2 relative px-4 py-6 flex flex-col justify-end items-center rounded-3xl transition-all";
+  const bg =
+    selectedPokemon?.id === pokemon.id
+      ? "border-indigo-400"
+      : "border-slate-200";
   return (
     <div
       onClick={() => {
         setSelectedPokemon(pokemon);
       }}
-      className="cursor-pointer transition-shadow relative px-4 py-6 flex  flex-col justify-end items-center rounded-3xl bg-white shadow-2xl shadow-slate-200 hover:shadow-slate-300"
+      className={`${container} ${bg} ${shadows} duration-75`}
     >
       <img
         className="absolute pixelated top-0 -translate-y-1/2 left-1/2 transform -translate-x-1/2"
